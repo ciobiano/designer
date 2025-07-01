d5f5e14f02fdfead8318a1531caf2988f0a1f9e4
ciobiano <rafaelobiano@yahoo.com>
2025-07-01 23:49:40 +0100

    feat: Add NextSection component and integrate it into the main application layout

---
+++ src/components/NextSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NextSection: React.FC = () => {
+  return (
+    <main >
+      <section className='about-sec'>
+        <div className='flex flex-col gap-64 p-64'>
+          <div className='flex flex-wrap items-stretch justify-start mb-3.5'>
+            <img src="/assets/monye1.jpg" alt="" />
+            
+
+          </div>
+
+        </div>
+
+
+      </section>
+    </main>
+  );
+};
+
+export default NextSection;
